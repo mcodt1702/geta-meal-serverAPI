df3619ef47457f984b36c8b0664b80a5240953f6
mcodt1702 <marco@mcnyc.net>
2020-09-11 14:01:46 -0400

    database and tables created and migrated

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
-const app = require("./app");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { CLIENT_ORIGIN } = require("./config");
 
-const { PORT } = require("./config");
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.get("/api/*", (req, res) => {
+  res.json({ ok: true });
 });
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
+module.exports = { app };
